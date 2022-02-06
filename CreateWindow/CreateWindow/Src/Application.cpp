aa612fff2d33cb19b215b9dcae137f856e11bed2
Billy Franscois <billyfranscois@outlook.com>
2022-02-06 23:54:27 +0900

    Update Application.cpp

--- CreateWindow/CreateWindow/Src/Application.cpp
+++ CreateWindow/CreateWindow/Src/Application.cpp
@@ -12,7 +12,7 @@ int main(void)
     // Hint desire value
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(640, 480, "My Application", nullptr, nullptr);
