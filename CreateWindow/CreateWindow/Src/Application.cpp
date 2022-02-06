7f579f4928fcb86b388f169fba682c55870bfb7c
Billy Franscois <billyfranscois@outlook.com>
2022-02-07 00:05:55 +0900

    Added a simple vertex Triangle

--- CreateWindow/CreateWindow/Src/Application.cpp
+++ CreateWindow/CreateWindow/Src/Application.cpp
@@ -10,9 +10,9 @@ int main(void)
         return -1;
 
     // Hint desire value
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(640, 480, "My Application", nullptr, nullptr);
@@ -27,11 +27,13 @@ int main(void)
     // Make the window's context current
     glfwMakeContextCurrent(window);
 
+    // Checking error while load window
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Error!" << std::endl;
     }
 
+    // Check OpenGL version
     std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 
     unsigned int value;
@@ -42,8 +44,24 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClearColor(0.5f, 0.3f, 0.7f, 1.0f); // Render color
+        glClearColor(0.2f, 0.3f, 0.7f, 1.0f); // Render color
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a simple triangle with vertex
+        glBegin(GL_TRIANGLES);
+
+        // Vertex 2D
+    	glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f); 
+
+        // Vertex 3D
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(0.0f, 0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+
+        glEnd();
+
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
