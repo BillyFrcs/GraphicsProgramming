f2923bc7520782bea23baad1abed4a24c3431965
Billy Franscois <billyfranscois@outlook.com>
2022-02-08 08:42:22 +0900

    Update VAO & ABO object with pointer

--- VertexTriangle/VertexTriangle/Src/Application.cpp
+++ VertexTriangle/VertexTriangle/Src/Application.cpp
@@ -11,7 +11,7 @@
 #include "VertexBuffer.h"
 #include "Buffer.h"
 
-// Windows screen
+// Window screen
 constexpr auto WIDTH = 640;
 constexpr auto HEIGHT = 480;
 
@@ -26,6 +26,7 @@ GLfloat vertices[] =
     0.0f, -0.5f * static_cast<float>(sqrt(3)) / 3, 0.0f,
 };
 
+// Vertices indices
 GLuint indices[] =
 {
     0, 3, 5, // Lower left triangle
@@ -71,16 +72,18 @@ int main(void)
 
     Shader ShaderProgram("Shaders/Vertex.vert", "Shaders/Fragment.frag");
 
-    VertexArray VAO;
-    VAO.Bind();
+    auto* VAO = new VertexArray();
+    VAO->Bind();
 
-    VertexBuffer VBO(vertices, sizeof(vertices));
+    VertexBuffer* VBO = new VertexBuffer(vertices, sizeof(vertices));
+
     Buffer EBO(indices, sizeof(indices));
 
-    VAO.LinkVBO(VBO, 0);
+    VAO->LinkVBO(*VBO, 0);
 
-    VAO.Unbind();
-    VBO.Unbind();
+    VAO->Unbind();
+
+    VBO->Unbind();
     EBO.Unbind();
 
     glfwSwapBuffers(window);
@@ -93,7 +96,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
         ShaderProgram.Activate();
 
-        VAO.Bind();
+        VAO->Bind();
 
         // Draw the triangle
         // glDrawArrays(GL_TRIANGLES, 0, 3); // Draw default triangle
@@ -107,8 +110,8 @@ int main(void)
     }
 
     // Delete all the objects
-    VAO.Delete();
-    VBO.Delete();
+    VAO->Delete();
+    VBO->Delete();
     EBO.Delete();
 
     ShaderProgram.Delete();
