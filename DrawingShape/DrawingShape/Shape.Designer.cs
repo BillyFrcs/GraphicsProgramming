e13cf1e4f59e3594544c9bc0b7861cabb5de32d6
Billy Franscois <BillyFranscois19@outlook.com>
2022-04-16 12:15:53 +0900

    Added Drawing Shape

---
+++ DrawingShape/DrawingShape/Shape.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DrawingShape
+{
+    partial class Shape
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawingShapePanel = new System.Windows.Forms.Panel();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.DrawingShapePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DrawingShapePanel
+            // 
+            this.DrawingShapePanel.Controls.Add(this.ExitButton);
+            this.DrawingShapePanel.Location = new System.Drawing.Point(-1, 0);
+            this.DrawingShapePanel.Name = "DrawingShapePanel";
+            this.DrawingShapePanel.Size = new System.Drawing.Size(464, 385);
+            this.DrawingShapePanel.TabIndex = 0;
+            this.DrawingShapePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawingShapePanelPaint);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(369, 343);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(82, 29);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButtonClick);
+            // 
+            // Shape
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 384);
+            this.Controls.Add(this.DrawingShapePanel);
+            this.Name = "Shape";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shape";
+            this.DrawingShapePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel DrawingShapePanel;
+        private Button ExitButton;
+    }
+}
---
+++ DrawingShape/DrawingShape/Shape.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingShape
+{
+    public partial class Shape : Form
+    {
+        private readonly float _x;
+        private readonly float _y;
+        private readonly float _width;
+        private readonly float _height;
+
+        public Shape()
+        {
+            // Initialize
+            _x = 20f;
+            _y = 20f;
+            _width = 100f;
+            _height = 100f;
+
+            InitializeComponent();
+        }
+
+        private void DrawingShapePanelPaint(object sender, PaintEventArgs paintEvent)
+        {
+            Graphics graphics = paintEvent.Graphics;
+
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Rectangle rectangle = new Rectangle((int)_x, (int)_y, (int)_width, (int)_height);
+
+            const int penWidth = 1;
+
+            Pen colorPen = new Pen(Color.Orange, penWidth);
+            Pen line = new Pen(Color.Black, penWidth);
+
+            SolidBrush solidColorBrush = new SolidBrush(Color.DarkMagenta);
+
+            graphics.DrawRectangle(colorPen, rectangle);
+            graphics.FillEllipse(solidColorBrush, rectangle);
+            graphics.DrawLine(line, 0, 250, this.Width, 250);
+
+            DrawCircle(graphics);
+        }
+
+        /// <summary>
+        /// Draw simple circle with color
+        /// </summary>
+        /// <param name="graphics">Graphics</param>
+        private void DrawCircle(Graphics graphics)
+        {
+            graphics.FillEllipse(Brushes.Blue, 70, 220, 30, 30);
+            graphics.FillEllipse(Brushes.SkyBlue, 100, 210, 40, 40);
+            graphics.FillEllipse(Brushes.PaleGreen, 140, 200, 50, 50);
+            graphics.FillEllipse(Brushes.Yellow, 190, 190, 60, 60);
+            graphics.FillEllipse(Brushes.Violet, 250, 180, 70, 70);
+            graphics.FillEllipse(Brushes.Brown, 320, 170, 80, 80);
+        }
+
+        private void ExitButtonClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
