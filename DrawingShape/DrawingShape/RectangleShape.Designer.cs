554f0b5c7761e7296366b00e968826d8a689718a
Billy Franscois <BillyFranscois19@outlook.com>
2022-04-17 12:19:32 +0900

    Added Simple Rectangle Shape

--- DrawingShape/DrawingShape/RectangleShape.Designer.cs
+++ DrawingShape/DrawingShape/RectangleShape.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DrawingShape
 {
-    partial class Shape
+    partial class RectangleShape
     {
         /// <summary>
         /// Required designer variable.
@@ -28,47 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DrawingShapePanel = new System.Windows.Forms.Panel();
+            this.RectanglePanel = new System.Windows.Forms.Panel();
             this.ExitButton = new System.Windows.Forms.Button();
-            this.DrawingShapePanel.SuspendLayout();
+            this.RectanglePanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // DrawingShapePanel
+            // RectanglePanel
             // 
-            this.DrawingShapePanel.Controls.Add(this.ExitButton);
-            this.DrawingShapePanel.Location = new System.Drawing.Point(-1, 0);
-            this.DrawingShapePanel.Name = "DrawingShapePanel";
-            this.DrawingShapePanel.Size = new System.Drawing.Size(465, 385);
-            this.DrawingShapePanel.TabIndex = 0;
-            this.DrawingShapePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawingShapePanelPaint);
+            this.RectanglePanel.Controls.Add(this.ExitButton);
+            this.RectanglePanel.Location = new System.Drawing.Point(12, 12);
+            this.RectanglePanel.Name = "RectanglePanel";
+            this.RectanglePanel.Size = new System.Drawing.Size(441, 361);
+            this.RectanglePanel.TabIndex = 0;
+            this.RectanglePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.RectanglePanelPaint);
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(369, 343);
+            this.ExitButton.Location = new System.Drawing.Point(349, 324);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(82, 29);
+            this.ExitButton.Size = new System.Drawing.Size(89, 34);
             this.ExitButton.TabIndex = 0;
             this.ExitButton.Text = "Exit";
             this.ExitButton.UseVisualStyleBackColor = true;
             this.ExitButton.Click += new System.EventHandler(this.ExitButtonClick);
             // 
-            // Shape
+            // RectangleShape
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 384);
-            this.Controls.Add(this.DrawingShapePanel);
-            this.Name = "Shape";
+            this.ClientSize = new System.Drawing.Size(465, 385);
+            this.Controls.Add(this.RectanglePanel);
+            this.Name = "RectangleShape";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Shape";
-            this.DrawingShapePanel.ResumeLayout(false);
+            this.Text = "Rectangle";
+            this.RectanglePanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Panel DrawingShapePanel;
+        private Panel RectanglePanel;
         private Button ExitButton;
     }
 }
---
+++ DrawingShape/DrawingShape/RectangleShape.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingShape
+{
+    public partial class RectangleShape : Form
+    {
+        private Graphics _Graphics;
+
+        private Pen _PenColor = new(Color.Blue, 5f);
+
+        public RectangleShape()
+        {
+            InitializeComponent();
+        }
+
+        ~RectangleShape()
+        {
+            this._PenColor.Dispose();
+            this._Graphics.Dispose();
+        }
+
+        private void RectanglePanelPaint(object sender, PaintEventArgs e)
+        {
+            this._Graphics = e.Graphics;
+
+            RectangleF[] rectangleF =
+            {
+                new RectangleF(90f, 30f, 250f, 250f),
+
+                // new RectangleF(120f, 60f, 190f, 190f),
+            };
+
+            var brush1 = new SolidBrush(Color.Orange);
+            var brush2 = new SolidBrush(Color.GreenYellow);
+
+            this._Graphics.DrawRectangles(this._PenColor, rectangleF);
+
+            const int x = 110;
+            const int y = 50;
+            const int width = 120;
+            const int height = 120;
+
+            // DrawEllipse(x, y, width, height, brush1, brush2);
+
+            DrawRectangle(x, y, 130, 130, ref brush1, ref brush2);
+        }
+
+        /// <summary>
+        /// Draw Circles
+        /// </summary>
+        /// <param name="x">X position</param>
+        /// <param name="y">Y position</param>
+        /// <param name="width">Width of circle</param>
+        /// <param name="height">Height of circle</param>
+        /// <param name="brush1">Solid color 1</param>
+        /// <param name="brush2">Solid color 2</param>
+        private void DrawEllipse(in int x, in int y, in int width, in int height, in SolidBrush brush1, in SolidBrush brush2)
+        {
+            Rectangle rectangle1 = new(x, y, width, height);
+            Rectangle rectangle2 = new(190, 150, width, height);
+
+            this._Graphics.FillEllipse(brush1, rectangle1);
+            this._Graphics.FillEllipse(brush2, rectangle2);
+        }
+
+        /// <summary>
+        /// Draw rectangle
+        /// </summary>
+        /// <param name="x">X position</param>
+        /// <param name="y">Y position</param>
+        /// <param name="width">Width of rectangle</param>
+        /// <param name="height">Height of rectangle</param>
+        /// <param name="brush1">Solid color 1</param>
+        /// <param name="brush2">Solid color 2</param>
+        private void DrawRectangle(in int x, in int y, in int width, in int height, ref SolidBrush brush1, ref SolidBrush brush2)
+        {
+            Rectangle[] rectangle1 =
+            { 
+                new(x, y, width, height)
+            };
+
+            Rectangle[] rectangle2 =
+            {
+                new(190, 140, width, height)
+            };
+            
+            _Graphics.FillRectangles(brush1, rectangle1);
+            _Graphics.FillRectangles(brush2, rectangle2);
+        }
+
+        private void ExitButtonClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
